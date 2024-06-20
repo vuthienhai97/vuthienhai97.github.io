20a460251980434083c9c4dd169284634a8b4571
vuthienhai97gh <vuthienhai97@gmail.com>
2024-06-20 13:41:16 +0700

    change vuthienhai97gh to vuthienhai97

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -2,7 +2,7 @@ export const contactsData = {
   email: 'vuthienhai97@gmail.com',
   phone: '+84 762588793',
   address: 'District Go Vap, Ho Chi Minh city ',
-  github: 'https://github.com/vuthienhai97gh',
+  github: 'https://github.com/vuthienhai97',
   linkedIn: 'https://www.linkedin.com/in/hai-vu-thien-865a62209/',
   // twitter: 'https://twitter.com/said7388',
   stackOverflow: 'https://stackoverflow.com/users/16288240/vuthienhai97/',
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -7,7 +7,7 @@ export const personalData = {
   email: 'vuthienhai97@gmail.com',
   phone: '+84 762588793',
   address: 'District Go Vap, Ho Chi Minh city',
-  github: 'https://github.com/vuthienhai97gh',
+  github: 'https://github.com/vuthienhai97',
   linkedIn: 'https://www.linkedin.com/in/hai-vu-thien-865a62209/',
   stackOverflow: 'https://stackoverflow.com/users/16288240/vuthienhai97/',
   hackerrank: 'https://www.hackerrank.com/profile/vuthienhai97',
